9be9b57508e6660eb16b80b4fbaea0f6e89cc751
Aleksi Statsevich <aleksi.statsevich@gmail.com>
2020-11-14 15:36:38 +0200

    firestore works now

--- src/component/Dashboard.js
+++ src/component/Dashboard.js
@@ -3,6 +3,9 @@ import {Card, Button, Alert} from 'react-bootstrap'
 import { useAuth } from '../context/AuthContext'
 import { Link, useHistory } from "react-router-dom"
 import AddIcon from '@material-ui/icons/Add';
+import Tooltip from '@material-ui/core/Tooltip';
+import Fab from '@material-ui/core/Fab';
+import firebase from '../firebase'
 
 export default function Dashboard() {
     const [error, setError] = useState("")
@@ -22,17 +25,34 @@ export default function Dashboard() {
         }
     }
 
+    async function newGame() {
+        setError("")
+        const data = {
+            home: "pats",
+            away: "seattle"
+        }
+        try {
+            await firebase.firestore().collection('games').add(data);
+        } catch(error) {
+            console.log(error)
+            setLoading(false)
+            return setError("Creating new game failed")
+        }
+    }
+
     return (
         <>  
-            <Tooltip title="Add" aria-label="add">
-                <Fab color="secondary" className={classes.absolute}>
-                <AddIcon />
+            {error && <Alert variant="danger">{error}</Alert>}
+            <Tooltip title="Add" aria-label="add" onClick={newGame}>
+                <Fab color="primary">
+                    <AddIcon />
                 </Fab>
             </Tooltip>
+            New game
+
             {/* <Card>
                 <Card.Body> */}
                     {/* <h2 className="text-center mb-4">Profile</h2>
-                    {error && <Alert variant="danger">{error}</Alert>}
                     <strong>Email: </strong>{currentUser.email}
                    <Link className="btn btn-primary w-100 mt-3" to="/update-profile">Update profile</Link> */}
                 {/* </Card.Body>
