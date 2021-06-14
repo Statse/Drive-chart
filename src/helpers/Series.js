b34b631e072e7e470be9fbc917ae6f1b1a8e664b
Aleksi Statsevich <aleksi.statsevich@gmail.com>
2021-06-14 23:19:43 +0300

    series mapper 0.1

--- src/helpers/Series.js
+++ src/helpers/Series.js
@@ -6,24 +6,17 @@ export default function SeriesMappers(downs) {
     let currentQuater = downs[0].quarter
 
     downs.map((down, index)=>{
-
-        if (currentTeam !== down.possession || currentQuater === 3 && down.quarter === 4){
+        if (currentTeam !== down.possession || (currentQuater === 3 && down.quarter === 4)){
             //change series possession
             currentTeam = down.possession 
-
-            console.log("currentSeries", currentSeries, "----------")
+            
             //push previous series to series
             series.push(currentSeries)
 
-            //reset currentSeries
+            //reset current series
             currentSeries = []
-        } else {
-            //if team is same as last down then keep the series
-            currentSeries.push(down)
-        }
+        } 
+        currentSeries.push(down)
     })
-
-    console.log("series", series)
-
     return series
 }
