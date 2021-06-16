f7a6ee7a1340a8b2a0bd52c032a0733cf8a8d3b0
Aleksi Statsevich <aleksi.statsevich@gmail.com>
2021-06-16 19:23:35 +0300

    first downs fix

--- src/component/Recap/Team/FirstDowns.js
+++ src/component/Recap/Team/FirstDowns.js
@@ -2,25 +2,37 @@ import React from 'react'
 import Typography from '@material-ui/core/Typography';
 import StatCard from '../Wrappers/StatCard'
 
+import SeriesMapper from  '../../../helpers/Series'
+
 export default function FirstDowns(props) {
     const {game, team} = props
     const {downs} = game
 
-    const firstDowns = downs.filter((down)=>{
-        if (down.possession.toLowerCase() === team){
-            if (down.playType !== "KO"){
-                if (down.down === 1){
-                    return down.down
-                }
-            }
+    const playSeries = SeriesMapper(downs)
+
+    let firstDowns = 0
+    let awayFirsts = 0
+
+    playSeries.map((series)=>{
+      let possession = ""
+      let firsts = series.filter((down)=>{
+        possession = down.possession
+        if (down.down === 1 && down.playType !== "KO" && down.playType !== "PAT"){
+          return down
         }
+      })
+
+      if (firsts.length > 0){
+        if (possession.toLowerCase() === team.toLowerCase()){
+          firstDowns += firsts.length - 1
+        } 
+      }
     })
-    
 
     return (
         <StatCard>
                 <Typography variant="h5" component="h2">First downs</Typography>
-                <Typography variant="h4" component="p">{firstDowns.length}</Typography>
+                <Typography variant="h4" component="p">{firstDowns}</Typography>
         </StatCard>
     )
 }
