803dea2830fb1029174a7429c8ca61b9a4e072b9
Aleksi Statsevich <aleksi.statsevich@gmail.com>
2021-06-27 13:30:52 +0300

    simplify routes

--- src/containers/GameRecapWrapper.js
+++ src/containers/GameRecapWrapper.js
@@ -1,12 +1,9 @@
 import React from 'react'
-import {GameProvider} from '../context/GameContext'
 import GameRecap  from './GameRecap'
 import {useGame} from '../context/GameContext'
 
 export default function GameRecapWrapper(props) {
     return (
-        <GameProvider>
-            <GameRecap {...props} useGame={useGame}/>
-        </GameProvider>
+        <GameRecap {...props} useGame={useGame}/>
     )
 }
--- src/containers/PlayerRecapWrapper.js
+++ src/containers/PlayerRecapWrapper.js
@@ -1,12 +1,9 @@
 import React from 'react'
-import {GameProvider} from '../context/GameContext'
 import PlayerRecap  from './PlayerRecap'
 import {useGame} from '../context/GameContext'
 
 export default function PlayerRecapWrapper(props) {
     return (
-        <GameProvider>
-            <PlayerRecap {...props} useGame={useGame}/>
-        </GameProvider>
+        <PlayerRecap {...props} useGame={useGame}/>
     )
 }
