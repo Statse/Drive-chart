736de1cef8beee425b5421a64fee7f8bc41b2838
Aleksi Statsevich <aleksi.statsevich@gmail.com>
2021-07-08 18:06:41 +0300

    env

--- src/firebase.js
+++ src/firebase.js
@@ -3,15 +3,17 @@ import "firebase/auth"
 import "firebase/firestore"
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBTHKiugJ78PVxaxSIWC38xFtBrquS0sVY",
-    authDomain: "drive-chart-df487.firebaseapp.com",
-    databaseURL: "https://drive-chart-df487.firebaseio.com",
-    projectId: "drive-chart-df487",
-    storageBucket: "drive-chart-df487.appspot.com",
-    messagingSenderId: "678582206392",
-    appId: "1:678582206392:web:c138e67f2c1f3a07ea994d",
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_DATABASE_AUTH_DOMAIN,
+    databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID,
     measurementId: "G-13584P8W4L"
 };
+
+console.log("process.env", process.env)
 
 const app = firebase.initializeApp(firebaseConfig)
 
